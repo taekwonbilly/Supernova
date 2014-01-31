6c96c84d7e1313a888b08784cfee36174f4d3da3
Abi Gopal <agopal43@gmail.com>
2014-01-31 13:57:57 -0500

    More readability (messed up line breaks... sorry)

--- src/com/idt/contest/highschool/winter2014/BuiltinTester.java
+++ src/com/idt/contest/highschool/winter2014/BuiltinTester.java
@@ -9,9 +9,11 @@ public class BuiltinTester {
 			return;
 		}
 	}
+
 	public static <A> void customAssert(A a, Function<A> f){
 		customAssert(a, f, "DefaultErrorMessage");
 	}
+
 	public static <A,B> void dualAssert(A a, B b, Function<A> f1, Function<B> f2, String errorMessage){
 		if(f1.test(a) && f2.test(b)){
 			// log the correct stuff
@@ -22,9 +24,11 @@ public class BuiltinTester {
 			return;
 		}
 	}
+
 	public static <A,B> void dualAssert(A a, B b, Function<A> f1, Function<B> f2){
 		dualAssert(a, b, f1, f2, "DefaultErrorMessage");
 	}
+
 	public static <A,B> void dualAssert(A a, B b, BinaryFunction<A,B> bf, String errorMessage){
 		if(bf.test(a, b)){
 			// log correct stuff
@@ -35,13 +39,16 @@ public class BuiltinTester {
 			return;
 		}
 	}	
+
 	public static <A,B> void dualAssert(A a, B b, BinaryFunction<A,B> bf){
 		dualAssert(a, b, bf, "DefaultErrorMessage");
 	}
+
 	public static <A> void assertEquals(A a1, A a2, String errorMessage){
 		// do stuff (needs Builder)
 		return;
 	}
+
 	public static <A> void assertEquals(A a1, A a2){
 		assertEquals(a1, a2, "DefaultErrorMessage");
 	}
--- src/com/idt/contest/highschool/winter2014/Equals.java
+++ src/com/idt/contest/highschool/winter2014/Equals.java
@@ -1,8 +1,10 @@
 public class Equals<A> implements Function<A>{
 	A toTest;
+
 	public Equals(A a){
 		this.toTest = a;
 	}
+
 	public boolean test(A a){
 		return this.toTest.equals(a);
 	}
