183e8de51708819ccb181de524427ae74eeaad46
Abi Gopal <agopal43@gmail.com>
2014-02-01 18:28:18 -0500

    Added documentation to Function and BinaryFunction

--- src/com/idt/contest/highschool/winter2014/BinaryFunction.java
+++ src/com/idt/contest/highschool/winter2014/BinaryFunction.java
@@ -1,3 +1,11 @@
+/**
+ * Can be used instead of Function to pass two objects to the same testing function. See the Builder class for some predefined BinaryFunctions.
+ */
 public interface BinaryFunction<A,B>{
+	/**
+	 * Similar to Function's test, except takes two objects instead. 
+	 * @param a An object to be tested.
+	 * @param b Another object to be tested.
+	 */
 	public boolean test(A a, B b);
 }
