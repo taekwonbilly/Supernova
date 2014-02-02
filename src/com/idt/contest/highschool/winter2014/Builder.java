57454c616f9666c3dcce1ddb249d56b0b500757b
Abi Gopal <agopal43@gmail.com>
2014-02-01 20:19:35 -0500

    Inserted documentation for Builder

--- src/com/idt/contest/highschool/winter2014/Builder.java
+++ src/com/idt/contest/highschool/winter2014/Builder.java
@@ -1,5 +1,14 @@
 package com.idt.contest.highschool.winter2014;
+/** 
+ * Handy prebuilt Functions ready to be used with BuiltinTester.
+ */
 public class Builder {
+
+	/**
+	 * Returns a Function to test if an array of integers is in order.
+	 * @param An array of integers.
+	 * @return Function to test if an array is in order.
+	 */
 	public static Function inOrder(int[] a){
 		return new Function<int[]>(){
 			public boolean test(int[] t){
@@ -12,6 +21,11 @@ public class Builder {
 		};
 	}
 	
+	/**
+	 * Returns a Function to test if an array of doubles is in order.
+	 * @param An array of doubles.
+	 * @return Function to test if an array is in order.
+	 */
 	public static Function inOrder(double[] a){
 		return new Function<double[]>(){
 			public boolean test(double[] t){
@@ -23,7 +37,12 @@ public class Builder {
 			}
 		};
 	}
-
+	
+	/**
+	 * Returns a Function to test if an array of Comparables is in order.
+	 * @param An array of Comparables.
+	 * @return Function to test if an array is in order.
+	 */
 	public static Function<Comparable<Object>[]> inOrder(Comparable<Object>[] a){
 		return new Function<Comparable<Object>[]>(){
 			public boolean test(Comparable<Object>[] t){
@@ -36,6 +55,10 @@ public class Builder {
 		};
 	}
 
+	/**
+	 * Returns a Function to test if an Object is null.
+	 * @return Function to test if an Object is null.
+	 */
 	public static Function<Object> isNull(){
 		return new Function<Object>(){
 			public boolean test(Object a){
@@ -44,6 +67,10 @@ public class Builder {
 		};
 	}
 
+	/**
+	 * Returns a Function to test if an Object is not null.
+	 * @return Function to test if an Object is not null.
+	 */
 	public static Function<Object> isNotNull(){
 		return new Function<Object>(){
 			public boolean test(Object a){
@@ -52,6 +79,11 @@ public class Builder {
 		};
 	}
 
+	/**
+	 * Returns a Function to test if an Object equals another object.
+	 * @param t An object to be compared to.
+	 * @return Function to test if an Object is equal to t.
+	 */
 	public static Function<Comparable<Object>> equals(Comparable<Object> t){
 		final Comparable<Object> b = t;
 		return new Function<Comparable<Object>>(){
@@ -61,6 +93,11 @@ public class Builder {
 		};
 	}
 	
+	/**
+	 * Returns a Function to test if an Object is strictly greater than another object.
+	 * @param t An object to be compared to.
+	 * @return Function to test if an Object is strictly to t.
+	 */
 	public static Function<Comparable<Object>> isGreater(Comparable<Object> t){
 		final Comparable<Object> b = t;
 		return new Function<Comparable<Object>>(){
