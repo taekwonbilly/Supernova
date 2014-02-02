75f262b43a2e9aad96073195ff69dff89a0b90cb
Abi Gopal <agopal43@gmail.com>
2014-02-01 20:36:25 -0500

    Added documentation to Equals and Expectation

--- src/com/idt/contest/highschool/winter2014/Equals.java
+++ src/com/idt/contest/highschool/winter2014/Equals.java
@@ -1,11 +1,24 @@
 package com.idt.contest.highschool.winter2014;
+/**
+ * A Function to test equality. Isolated from Builder, because it is a fundamental component.
+ */
 public class Equals<A> implements Function<A>{
+	/**
+	 * Generic to be tested.
+	 */
 	A toTest;
 
+	/**
+	 * Set the Generic that will be tested.
+	 */
 	public Equals(A a){
 		this.toTest = a;
 	}
 
+	/**
+	 * Test method required by Function.
+	 * @param a Generic that will be tested against toTest.
+	 */
 	public boolean test(A a){
 		return this.toTest.equals(a);
 	}
--- src/com/idt/contest/highschool/winter2014/Expectation.java
+++ src/com/idt/contest/highschool/winter2014/Expectation.java
@@ -1,15 +1,38 @@
 package com.idt.contest.highschool.winter2014;
 
+/**
+ * The Expectation object tests a particular object using a function to make sure that the result is correct.
+ */
 public class Expectation<A> {
+	/**
+	 *  Used to determine whether or not testing should be active. If testing not active, don't run any of this.
+	 */
 	boolean initCondition;
+	/**
+	 * Used to validate a particular object.
+	 */
 	Function<A> finalCondition;
+	/**
+	 * Location in stack to provide robust error logging.
+	 */
 	StackTraceElement creationLocation;
+	/**
+	 * Constructor for Expectation objects.
+	 * @param creationLocation Part of stack where it was created for robust error logging.
+	 * @param init Boolean to determine whether or not the testing framework should be active.
+	 * @param fin Function to test objects against.
+	 */
 	public Expectation(StackTraceElement creationLocation, boolean init, Function<A> fin){
 		this.creationLocation = creationLocation;
 		this.initCondition = init;
 		this.finalCondition = fin;
 	}
 
+	/**
+	 * Verifies that a passed object, when acted upon by fin is true. If not, error log. 
+	 * @param a Object to test.
+	 * @return Returns the object passed in.
+	 */
 	public A verify(A a){
 		String message;
 		boolean failed;
